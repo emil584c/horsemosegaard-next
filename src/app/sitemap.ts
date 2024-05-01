7d17e15d59f1ae6e3678cc742ec519fd6bb3386c
emil584c <105641442+emil584c@users.noreply.github.com>
2024-05-01 14:58:51 +0200

    site map update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://horsemosegaard.com",
+      url: "https://www.horsemosegaard.com",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://horsemosegaard.com/priser",
+      url: "https://www.horsemosegaard.com/priser",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://horsemosegaard.com/faciliteter",
+      url: "https://www.horsemosegaard.com/faciliteter",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.6,
     },
     {
-      url: "https://horsemosegaard.com/hvem-er-vi",
+      url: "https://www.horsemosegaard.com/hvem-er-vi",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.4,
